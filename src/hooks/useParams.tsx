c68fe7fb46ca6531dec68b8aed02fd3883ea3f06
Howl <howl@Howls-MacBook-Pro.local>
2023-12-19 03:10:39 -0800

    feat: support useParams

---
+++ src/hooks/useParams.tsx
@@ -0,0 +1,14 @@
+import { NavigationRouteContext } from '@react-navigation/native';
+import { useContext } from 'react';
+
+const useParams = <T extends Record<string, any> = {}>(): Readonly<T> => {
+  const route = useContext(NavigationRouteContext);
+  if (route === undefined) {
+    throw new Error(
+      "Couldn't find a route object. Is your component inside a screen in a navigator?"
+    );
+  }
+  return (route.params || {}) as T;
+};
+
+export default useParams;
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ export type { RouterRootProps } from './RouterRoot';
 export default RouterRoot;
 
 export { default as useRouter } from './hooks/useRouter';
+export { default as useParams } from './hooks/useParams';
 export { default as Stack } from './navigator/Stack';
 export { default as Tabs } from './navigator/Tabs';
 export { createNavigatorFactory } from './navigator/createNavigatorFactory';
